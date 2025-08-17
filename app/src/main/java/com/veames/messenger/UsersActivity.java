6532799e2defe528278537bd67864006dfb7e1b5
Pavel <paulsema52@gmail.com>
2025-08-18 02:07:01 +0700

    Added UsersActivity and UsersViewModel

--- app/src/main/java/com/veames/messenger/UsersActivity.java
+++ app/src/main/java/com/veames/messenger/UsersActivity.java
@@ -1,14 +1,25 @@
 package com.veames.messenger;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.google.firebase.auth.FirebaseUser;
 
 public class UsersActivity extends AppCompatActivity {
+
+    private UsersViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,39 @@ public class UsersActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        viewModel = new ViewModelProvider(this).get(UsersViewModel.class);
+        observeViewModel();
+    }
+
+    private void observeViewModel() {
+        viewModel.getUser().observe(this, new Observer<FirebaseUser>() {
+            @Override
+            public void onChanged(FirebaseUser firebaseUser) {
+                if (firebaseUser == null) {
+                    Intent intent = LoginActivity.newIntent(UsersActivity.this);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+    }
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, UsersActivity.class);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.item_logout) {
+            viewModel.logout();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/veames/messenger/UsersViewModel.java
+++ app/src/main/java/com/veames/messenger/UsersViewModel.java
@@ -1,4 +1,36 @@
 package com.veames.messenger;
 
-public class UsersViewModel {
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class UsersViewModel extends ViewModel {
+
+    private FirebaseAuth auth;
+
+    private MutableLiveData<FirebaseUser> user = new MutableLiveData<>();
+
+    public UsersViewModel() {
+        auth = FirebaseAuth.getInstance();
+        auth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                user.setValue(firebaseAuth.getCurrentUser());
+
+            }
+        });
+    }
+
+    public LiveData<FirebaseUser> getUser() {
+        return user;
+    }
+
+    public void logout() {
+        auth.signOut();
+    }
+
 }
