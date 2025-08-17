4cb69153faa1b66624c21105bbb90e6a99b14ef3
Pavel <paulsema52@gmail.com>
2025-08-18 02:06:15 +0700

    Added ReserPasswordActivity and ResetPasswordViewModel

--- app/src/main/java/com/veames/messenger/ResetPasswordActivity.java
+++ app/src/main/java/com/veames/messenger/ResetPasswordActivity.java
@@ -1,14 +1,29 @@
 package com.veames.messenger;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 public class ResetPasswordActivity extends AppCompatActivity {
+
+    private static final String EXTRA_EMAIL = "email";
+
+    private EditText editTextEmail;
+    private Button buttonResetPassword;
+
+    private ResetPasswordViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,65 @@ public class ResetPasswordActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        viewModel = new ViewModelProvider(this).get(ResetPasswordViewModel.class);
+        initViews();
+        observeViewModel();
+
+        String email = getIntent().getStringExtra(EXTRA_EMAIL);
+        editTextEmail.setText(email);
+
+        buttonResetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editTextEmail.getText().toString().trim();
+                if (email.isEmpty()) {
+                    Toast.makeText(
+                            ResetPasswordActivity.this,
+                            R.string.error_empty_fields,
+                            Toast.LENGTH_SHORT
+                    ).show();
+                } else {
+                    viewModel.resetPassword(email);
+                }
+            }
+        });
+    }
+
+    private void observeViewModel() {
+        viewModel.isSuccess().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean isSucceeded) {
+                if (isSucceeded) {
+                    Toast.makeText(
+                            ResetPasswordActivity.this,
+                            R.string.reset_link_sent,
+                            Toast.LENGTH_SHORT
+                    ).show();
+                }
+            }
+        });
+        viewModel.getError().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String errorMessage) {
+                if (errorMessage != null) {
+                    Toast.makeText(
+                            ResetPasswordActivity.this,
+                            errorMessage,
+                            Toast.LENGTH_SHORT
+                    ).show();
+                }
+            }
+        });
+    }
+
+    public static Intent newIntent(Context context, String email) {
+        Intent intent = new Intent(context, ResetPasswordActivity.class);
+        intent.putExtra(EXTRA_EMAIL, email);
+        return intent;
+    }
+
+    private void initViews() {
+        editTextEmail = findViewById(R.id.editTextEmail);
+        buttonResetPassword = findViewById(R.id.buttonResetPassword);
     }
 }
--- app/src/main/java/com/veames/messenger/ResetPasswordViewModel.java
+++ app/src/main/java/com/veames/messenger/ResetPasswordViewModel.java
@@ -1,4 +1,53 @@
 package com.veames.messenger;
 
-public class ResetPasswordViewModel {
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ResetPasswordViewModel extends ViewModel {
+
+    private static final String LOG_TAG = "ResetPasswordViewModel";
+
+    private FirebaseAuth auth;
+
+    private MutableLiveData<String> error = new MutableLiveData<>();
+    private MutableLiveData<Boolean> success = new MutableLiveData<>();
+
+    public ResetPasswordViewModel() {
+        auth = FirebaseAuth.getInstance();
+    }
+
+    public LiveData<String> getError() {
+        return error;
+    }
+
+    public LiveData<Boolean> isSuccess() {
+        return success;
+    }
+
+    public void resetPassword(String email) {
+        auth.sendPasswordResetEmail(email)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d(LOG_TAG, "Reset link has been successfully sen");
+                        success.setValue(true);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(LOG_TAG, e.toString());
+                        error.setValue(e.getMessage());
+                    }
+                });
+    }
 }
