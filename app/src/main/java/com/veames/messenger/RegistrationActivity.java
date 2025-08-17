f7a3f279f95a88f22d54be370a5a0fefae7d6288
Pavel <paulsema52@gmail.com>
2025-08-18 02:05:21 +0700

    Added RegistrationActivity and RegistrationViewModel

--- app/src/main/java/com/veames/messenger/RegistrationActivity.java
+++ app/src/main/java/com/veames/messenger/RegistrationActivity.java
@@ -1,14 +1,33 @@
 package com.veames.messenger;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private EditText editTextName;
+    private EditText editTextLastName;
+    private EditText editTextAge;
+    private Button buttonSignUp;
+
+    private RegistrationViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +39,76 @@ public class RegistrationActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        viewModel = new ViewModelProvider(this).get(RegistrationViewModel.class);
+        initViews();
+        observeViewModel();
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = getTrimmedValue(editTextEmail);
+                String password = getTrimmedValue(editTextPassword);
+                String name = getTrimmedValue(editTextName);
+                String lastName = getTrimmedValue(editTextLastName);
+                String ageText = getTrimmedValue(editTextAge);
+                if (
+                        email.isEmpty() ||
+                                password.isEmpty() ||
+                                name.isEmpty() ||
+                                lastName.isEmpty() ||
+                                ageText.isEmpty()
+                ) {
+                    Toast.makeText(
+                            RegistrationActivity.this,
+                            R.string.error_empty_fields,
+                            Toast.LENGTH_SHORT
+                    ).show();
+                } else {
+                    int age = Integer.parseInt(ageText);
+                    viewModel.signUp(email, password, name, lastName, age);
+                }
+            }
+        });
+    }
+
+    private void observeViewModel() {
+        viewModel.getError().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String errorMessage) {
+                if (errorMessage != null) {
+                    Toast.makeText(
+                            RegistrationActivity.this,
+                            errorMessage,
+                            Toast.LENGTH_SHORT
+                    ).show();
+                }
+            }
+        });
+        viewModel.getUser().observe(this, new Observer<FirebaseUser>() {
+            @Override
+            public void onChanged(FirebaseUser firebaseUser) {
+                if (firebaseUser != null) {
+                    Intent intent = UsersActivity.newIntent(RegistrationActivity.this);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+    }
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, RegistrationActivity.class);
+    }
+
+    private String getTrimmedValue(EditText editText) {
+        return editText.getText().toString().trim();
+    }
+
+    private void initViews() {
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextName = findViewById(R.id.editTextName);
+        editTextLastName = findViewById(R.id.editTextLastName);
+        editTextAge = findViewById(R.id.editTextAge);
+        buttonSignUp = findViewById(R.id.buttonSignUp);
     }
 }
--- app/src/main/java/com/veames/messenger/RegistrationViewModel.java
+++ app/src/main/java/com/veames/messenger/RegistrationViewModel.java
@@ -1,4 +1,61 @@
 package com.veames.messenger;
 
-public class RegistrationVIewModel {
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegistrationViewModel extends ViewModel {
+
+    private static final String LOG_TAG = "RegistrationViewModel";
+
+    private FirebaseAuth auth;
+
+    private MutableLiveData<String> error = new MutableLiveData<>();
+    private MutableLiveData<FirebaseUser> user = new MutableLiveData<>();
+
+    public RegistrationViewModel() {
+        auth = FirebaseAuth.getInstance();
+        auth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() != null) {
+                    user.setValue(firebaseAuth.getCurrentUser());
+                }
+            }
+        });
+    }
+
+    public LiveData<String> getError() {
+        return error;
+    }
+
+    public LiveData<FirebaseUser> getUser() {
+        return user;
+    }
+
+    public void signUp(String email, String password, String name, String lastName, int age) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Log.d(LOG_TAG, "User signed up");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(LOG_TAG, e.toString());
+                        error.setValue(e.getMessage());
+                    }
+                });
+    }
 }
